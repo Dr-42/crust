b8337f086ea3bbb480a8870fb708127a1369f876
Spandan Roy <royspandan1@gmail.com>
2024-02-22 18:16:30 +0530

    Create a separate preprocessor module

--- src/ast.rs
+++ src/ast.rs
@@ -4,42 +4,12 @@ use crate::parser;
 
 pub mod decldata;
 pub mod nodes;
+pub mod preprocess;
 
 pub type Span = codespan::Span;
 
-pub fn preremove_comments(text: String) -> String {
-    let mut result = String::new();
-    let mut chars = text.chars().peekable();
-    while let Some(c) = chars.next() {
-        if c == '/' {
-            if let Some(&'/') = chars.peek() {
-                chars.next();
-                for c in chars.by_ref() {
-                    if c == '\n' {
-                        break;
-                    }
-                }
-                continue;
-            } else if let Some(&'*') = chars.peek() {
-                chars.next();
-                while let Some(c) = chars.next() {
-                    if c == '*' {
-                        if let Some(&'/') = chars.peek() {
-                            chars.next();
-                            break;
-                        }
-                    }
-                }
-                continue;
-            }
-        }
-        result.push(c);
-    }
-    result
-}
-
 pub fn parse(text: &str) -> Result<Box<nodes::Program>, Box<dyn Error>> {
-    let text = preremove_comments(text.to_string());
+    let text = preprocess::preremove_comments(text.to_string());
     let parser = parser::ProgramParser::new();
     let res = parser.parse(&text).map_err(|e| format!("{:?}", e))?;
     Ok(res)
---
+++ src/ast/preprocess.rs
@@ -0,0 +1,30 @@
+pub fn preremove_comments(text: String) -> String {
+    let mut result = String::new();
+    let mut chars = text.chars().peekable();
+    while let Some(c) = chars.next() {
+        if c == '/' {
+            if let Some(&'/') = chars.peek() {
+                chars.next();
+                for c in chars.by_ref() {
+                    if c == '\n' {
+                        break;
+                    }
+                }
+                continue;
+            } else if let Some(&'*') = chars.peek() {
+                chars.next();
+                while let Some(c) = chars.next() {
+                    if c == '*' {
+                        if let Some(&'/') = chars.peek() {
+                            chars.next();
+                            break;
+                        }
+                    }
+                }
+                continue;
+            }
+        }
+        result.push(c);
+    }
+    result
+}
